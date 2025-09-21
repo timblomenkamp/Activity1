abc33296ff4e9204de006eceac764c79bb358c22
Benjamin <bepoerschke@icloud.com>
2025-09-21 16:09:24 +0200

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by Benjamin Poerschke on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
